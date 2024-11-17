5776cc39f520993f90bf7f20050adf4e7b36212b
Alexander Peyser <peyser.alex@gmail.com>
2024-11-17 16:43:57 +0100

    v1.8: cleanup

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Alex Peyser <peyser.alex.public@gmail.com>
 pkgname=suspend-pci
 pkgver=1
-pkgrel=7
+pkgrel=8
 pkgdesc="Remove pci devices before suspend"
 arch=('any')
 url=""
@@ -10,7 +10,7 @@ depends=('bash' 'pciutils' 'grep' 'coreutils')
 makedepends=()
 source=(suspend-pci.sh suspend-pci.conf)
 backup=(etc/suspend-pci.conf)
-sha256sums=('86ab99045eff87e6ca911e668a5e093cf6d9b06a152b6f15d203ee6c1ac7e052'
+sha256sums=('aaa5c514e61ecaa73465c1146c0cb6b502c6c5cf3debe85f32cb2c553895244c'
             '6eba84d44578b4e9c18b91d31cb469f815e330347901e36eb12b094f73532173')
 
 build() {
@@ -18,7 +18,6 @@ build() {
 }
 
 package() {
-    pwd
     install -p -m 0755 -D -t "$pkgdir/usr/lib/systemd/system-sleep" suspend-pci
     install -p -m 0644 -D -t "$pkgdir/etc" suspend-pci.conf
 }
--- suspend-pci.sh
+++ suspend-pci.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
+# -*- mode: Shell-script; sh_shell: "bash"; tab-width: 2; -*-
 
-# /usr/lib/systemd/system-sleep
+# Goes into
+# /usr/lib/systemd/system-sleep/suspend-pci
+# Reads /etc/suspend-pci line by line for devices to `remove`
 
 pre() {
-    local -a devices
-    readarray -t devices </etc/suspend-pci.conf
-    local device
-    for device in "${devices[@]}"; do
-        # Filter out empty & comment lines
-        ! [[ $device =~ ^[[:space:]]*(#.*)?$ ]] || continue
-        # Read the matching device and remove
-        local pci=$(lspci -D | grep "$device" | cut -f1 -d\ )
-        echo 1 >/sys/bus/pci/devices/$pci/remove
-    done
+  # read /etc/suspend-pci.conf
+  local -a devices
+  readarray -t devices </etc/suspend-pci.conf
+  # remove each device associated with conf file line
+  local device
+  for device in "${devices[@]}"; do
+    # Filter out empty & comment lines
+    ! [[ $device =~ ^[[:space:]]*(#.*)?$ ]] ||
+      continue
+    # Read the matching device and remove
+    local pci=$(lspci -D | grep "$device" | cut -f1 -d\ )
+    echo 1 >/sys/bus/pci/devices/$pci/remove
+  done
 }
 
 post() {
-    echo 1 >/sys/bus/pci/rescan
+  # rescan the bus for all removed pci devices
+  echo 1 >/sys/bus/pci/rescan
 }
 
 case "$1" in
-    pre) pre;;
-    post) post;;
+  pre)  pre;;
+  post) post;;
 esac
